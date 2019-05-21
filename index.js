bb38670d56c60346df3ef272c9baf7ff47f8271f
matthew-collins1991 <matthew.collins1991@yahoo.com>
2019-05-21 15:06:42 +0100

    Added basic project card structure

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const header = document.querySelector('#project-cards')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    init()
+  })
+
+
+const projectCardDiv = document.querySelector('#project-cards')
+
+const projectCards = (projects) => {
+
+    projects.map(project => {
+        const projectDiv = document.createElement('div')
+        projectDiv.innerHTML =
+        `
+        <h1>${project.name}</h1>
+        <h3>${project.blurb}</h3>
+        <p>${project.description}</p>
+        <p>${project.stack}</p>
+        <br>
+        `
+        projectCardDiv.append(projectDiv)
+    })
+}
+
+const init = () => {
+    projectCards(projects)
+}
+
--- projectInfo.js
+++ projectInfo.js
@@ -1,8 +1,30 @@
-const projects={
-    Hirely: {
+const projects = [
+{
+        name: "Hirely",
         image: "url",
         blurb: "Hiring within the community", 
-        description: "A Ruby on Rails web application that allows users to sign up, log in, post items fro hire and hire other available items in the community."
-        
+        description: "A Ruby on Rails web application that allows users to sign up, log in, post items fro hire and hire other available items in the community.",
+        stack: "Ruby, SQLite, Ruby on Rails, Bootstrap"
+    },
+{
+        name: "Degrees of Separation",
+        image: "url",
+        blurb: "Hiring within the community", 
+        description: "A Ruby on Rails web application that allows users to sign up, log in, post items fro hire and hire other available items in the community.",
+        stack: "Ruby, SQLite, Ruby on Rails, Bootstrap"
+    },
+{
+        name: "Crawl Of Duty",
+        image: "url",
+        blurb: "Hiring within the community", 
+        description: "A Ruby on Rails web application that allows users to sign up, log in, post items fro hire and hire other available items in the community.",
+        stack: "Ruby, SQLite, Ruby on Rails, Bootstrap"
+    },
+{
+        name: "BillsHub",
+        image: "url",
+        blurb: "Hiring within the community", 
+        description: "A Ruby on Rails web application that allows users to sign up, log in, post items fro hire and hire other available items in the community.",
+        stack: "Ruby, SQLite, Ruby on Rails, Bootstrap"
     }
-}
+]
