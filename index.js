1c0e23abe1e8859c1e106fea0a38f068a15d60e0
matthew-collins1991 <matthew.collins1991@yahoo.com>
2019-05-21 15:29:08 +0100

    added semantic to project cards

--- index.js
+++ index.js
@@ -11,13 +11,29 @@ const projectCards = (projects) => {
 
     projects.map(project => {
         const projectDiv = document.createElement('div')
+        projectDiv.className = "ui link cards"
         projectDiv.innerHTML =
         `
-        <h1>${project.name}</h1>
-        <h3>${project.blurb}</h3>
-        <p>${project.description}</p>
-        <p>${project.stack}</p>
-        <br>
+        <div class="card">
+    <div class="image">
+      <img src="./${project.image}">
+    </div>
+    <div class="content">
+      <div class="header">${project.name}</div>
+      <div class="meta">
+        <a>${project.blurb}</a>
+      </div>
+      <div class="description">
+        ${project.description}
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="left floated">
+        ${project.stack}
+      </span>
+   
+    </div>
+  </div>
         `
         projectCardDiv.append(projectDiv)
     })
--- projectInfo.js
+++ projectInfo.js
@@ -1,28 +1,28 @@
 const projects = [
 {
         name: "Hirely",
-        image: "url",
+        image: "public/img/avatar.jpeg",
         blurb: "Hiring within the community", 
         description: "A Ruby on Rails web application that allows users to sign up, log in, post items fro hire and hire other available items in the community.",
         stack: "Ruby, SQLite, Ruby on Rails, Bootstrap"
     },
 {
         name: "Degrees of Separation",
-        image: "url",
+        image: "public/img/DegreesOfSeparation.png",
         blurb: "Hiring within the community", 
         description: "A Ruby on Rails web application that allows users to sign up, log in, post items fro hire and hire other available items in the community.",
         stack: "Ruby, SQLite, Ruby on Rails, Bootstrap"
     },
 {
         name: "Crawl Of Duty",
-        image: "url",
+        image: "public/img/crawlOfDuty.png",
         blurb: "Hiring within the community", 
         description: "A Ruby on Rails web application that allows users to sign up, log in, post items fro hire and hire other available items in the community.",
         stack: "Ruby, SQLite, Ruby on Rails, Bootstrap"
     },
 {
         name: "BillsHub",
-        image: "url",
+        image: "public/img/billshub.png",
         blurb: "Hiring within the community", 
         description: "A Ruby on Rails web application that allows users to sign up, log in, post items fro hire and hire other available items in the community.",
         stack: "Ruby, SQLite, Ruby on Rails, Bootstrap"
